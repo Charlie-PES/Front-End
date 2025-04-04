9bb1e0e6b46a23e9c0bab2e980b67187ed5b9567
Marcelo Augusto Felisberto Martins <112526955+MFelisberto@users.noreply.github.com>
2025-04-04 09:51:19 -0300

    tela de pesquisa

--- pets-adocao-frontend/src/components/Header.js
+++ pets-adocao-frontend/src/components/Header.js
@@ -1,31 +1,32 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import SearchBar from './SearchBar';
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
+    return (
+      <header className={styles.header}>
 
-      <div className={styles.logoContainer}>
-        <img src="\images\logo.png" alt="Logo" className={styles.logo}/>
-      </div>
+        <div className={styles.logoContainer}>
+          <img src="\images\logo.png" alt="Logo" className={styles.logo}/>
+        </div>
 
-      <nav className={styles.nav}>
-     
-        <Link to="/" className={styles.link}>
-          HOME
-        </Link>
-     
-        <Link to="/adotar" className={styles.link}>
-          ADOTAR
-        </Link>
-     
-        <Link to="/sobre" className={styles.link}>
-          SOBRE
-        </Link>
-     
-      </nav>
-    
-    </header>
+        <nav className={styles.nav}>
+          <Link to="/" className={styles.link}>
+            Home
+          </Link>
+      
+          <Link to="/adotar" className={styles.link}>
+            Adotar
+          </Link>
+      
+          <Link to="/sobre" className={styles.link}>
+            Sobre
+          </Link>
+        </nav>
+
+        <SearchBar />
+      
+      </header>
   );
 };
 
