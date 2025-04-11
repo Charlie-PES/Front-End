7527bf5baf88f13c7f83152ecb674ba085d33829
Marcelo Augusto Felisberto Martins <112526955+MFelisberto@users.noreply.github.com>
2025-04-10 22:18:42 -0300

    Mudanças

--- pets-adocao-frontend/src/components/Footer/Footer.js
+++ pets-adocao-frontend/src/components/Footer/Footer.js
@@ -5,28 +5,31 @@ import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
+      
       <div className={styles.left}>
         <p>Obrigado por adotar os pets!</p>
         <img src="/images/logo.png" alt="Logo" className={styles.logo} />
       </div>
 
       <div className={styles.center}>
-        <input type="email" placeholder="Insira seu email aqui" className={styles.emailInput} />
-        <button className={styles.registerBtn}>Se registre agora</button>
-
+       
         <div className={styles.links}>
+          
           <div>
             <h4>Ajuda</h4>
             <p>FAQ</p>
             <p>Serviço ao consumidor</p>
             <p>Contate nós</p>
           </div>
+          
           <div>
             <h4>Outros</h4>
             <p>Política de privacidade</p>
             <p>Inscrições</p>
           </div>
+        
         </div>
+      
       </div>
 
       <div className={styles.right}>
@@ -34,6 +37,7 @@ const Footer = () => {
         <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
         <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
       </div>
+    
     </footer>
   );
 };
--- pets-adocao-frontend/src/components/Header/Header.js
+++ pets-adocao-frontend/src/components/Header/Header.js
@@ -1,49 +1,45 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
-import SearchBar from '../SearchBar/SearchBar';
 import React from 'react';
 
 const Header = () => {
-    return (
-      <header className={styles.header}>
+  return (
+    <header className={styles.header}>
+     
+      <div className={styles.logoContainer}>
+        <img src="\images\logo.png" alt="Logo" className={styles.logo}/>
+      </div>
 
-        <div className={styles.logoContainer}>
-          <img src="\images\logo.png" alt="Logo" className={styles.logo}/>
-        </div>
-
-        <nav className={styles.nav}>
-          <Link to="/home" className={styles.link}>
-            Home
-          </Link>
-      
-          <Link to="/adotar" className={styles.link}>
-            Adotar
-          </Link>
-      
-          <Link to="/sobre" className={styles.link}>
-            Sobre
-          </Link>
-
-          <Link to="/mapa" className={styles.link}>
+      <nav className={styles.nav}>
+        <Link to="/home" className={styles.link}>
+          Home
+        </Link>
+  
+        <Link to="/adotar" className={styles.link}>
+          Adotar
+        </Link>
+  
+        <Link to="/sobre" className={styles.link}>
+          Sobre
+        </Link>
+  
+        <Link to="/mapa" className={styles.link}>
           Mapa
-          </Link>
-          
-          <Link to="/perfil" className={styles.link}>
-          Perfil
-          </Link>
-        </nav>
-
-        <div className={styles.rightSection}>
-          <SearchBar />
-          
-          <Link to="/cadastro" className={styles.loginLink}>
-            Logar/Registrar
-          </Link>
+        </Link>
         
-        </div>
+        <Link to="/perfil" className={styles.link}>
+          Perfil
+        </Link>
+      </nav>
+
+      <div className={styles.rightSection}>
+        <Link to="/cadastro" className={styles.loginLink}>
+          Logar/Registrar
+        </Link>
+      </div>
+
       
-      
-      </header>
+    </header>
   );
 };
 
